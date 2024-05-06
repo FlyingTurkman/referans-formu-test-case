b24db9faad98a34fcb6029881da2ba385e554b2b
FlyingTurkman <dytdenizarli@hotmail.com>
2024-05-06 14:46:12 +0300

    console.log deleted

--- app/page.tsx
+++ app/page.tsx
@@ -53,8 +53,6 @@ export default function Home() {
       setValue('phone', selectedReference.phone)
       setValue('reference', selectedReference.reference)
     }
-
-    console.log('selected', selectedReference)
   }, [selectedReference])
   return (
     <main>
